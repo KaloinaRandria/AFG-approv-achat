eb7c809e86732f7cc94a153c3b2034aeee819fe1
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-11-21 10:46:51 +0300

    init

---
+++ src/main/java/afg/achat/afgApprovAchat/AfgApprovAchatApplication.java
@@ -0,0 +1,13 @@
+package afg.achat.afgApprovAchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfgApprovAchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfgApprovAchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/afg/achat/afgApprovAchat/AfgApprovAchatApplicationTests.java
@@ -0,0 +1,13 @@
+package afg.achat.afgApprovAchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfgApprovAchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
