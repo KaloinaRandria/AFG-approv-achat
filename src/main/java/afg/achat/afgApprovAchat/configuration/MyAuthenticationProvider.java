68a98d27eed56890eda5b3ca1070dc0e0cdfa3d0
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-11-26 16:58:06 +0300

    Create MyAuthenticationProvider.java

---
+++ src/main/java/afg/achat/afgApprovAchat/configuration/MyAuthenticationProvider.java
@@ -0,0 +1,92 @@
+package afg.achat.afgApprovAchat.configuration;
+
+import afg.achat.afgApprovAchat.model.utilisateur.Utilisateur;
+import afg.achat.afgApprovAchat.service.utilisateur.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.naming.NamingException;
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider{
+
+	@Autowired
+	private ActiveDirectory activeDir ; 
+	
+    private UtilisateurService utilisateurService;
+	
+	@Override
+	public Authentication authenticate(Authentication arg0) throws AuthenticationException {
+		String email = arg0.getName().trim() ;
+		String password = arg0.getCredentials().toString().trim() ;
+		try {
+
+			/*if(email.startsWith("test") && password.equals(email)) {
+				Collaborateur user = ur.findByEmailActif(email);
+				return new UsernamePasswordAuthenticationToken(user, null,
+				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
+			}
+			if(email.equals("s.dago@atlantic-group.net") && password.equals("P@ssword01")) {
+				Collaborateur user = ur.findByEmailActif(email);
+				return new UsernamePasswordAuthenticationToken(user, null,
+				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
+			}
+		
+			if(email.equals("charmica.andriakotoarson@afgbank.mg") && password.equals("charmica.andriakotoarson@afgbank.mg")) {
+				Collaborateur user = ur.findByEmailActif(email);
+				return new UsernamePasswordAuthenticationToken(user, null,
+				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
+			}*/
+			/*if(email.equals("ina.diallo@afgbank.mg") && password.equals("ina.diallo@afgbank.mg")) {
+				Collaborateur user = ur.findByEmailActif(email);
+				return new UsernamePasswordAuthenticationToken(user, null,
+				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
+			}*/
+//			if(email.equals("fenohasina.andrianantoandro@afgbank.mg") && password.equals("fenohasina.andrianantoandro@afgbank.mg")) {
+//				Utilisateur user = utilisateurService.getUtilisateurByMail(email);
+//				return new UsernamePasswordAuthenticationToken(user, null,
+//				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
+//			}
+//
+//			if(email.equals("s.dago@atlantic-group.net") && password.equals("P@ssword01")) {
+//				Utilisateur user = utilisateurService.getUtilisateurByMail(email);
+//				return new UsernamePasswordAuthenticationToken(user, null,
+//				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
+//			}
+			if(activeDir.authentify(email, password)) {
+                Utilisateur user = utilisateurService.getUtilisateurByMail(email);
+                if (user == null) {
+                    throw new BadCredentialsException("Utilisateur non autorisé");
+                }
+
+                Collection<GrantedAuthority> authorities = user.getRoles().stream()
+                        .map(role ->(GrantedAuthority) new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                        .toList();
+
+                return new UsernamePasswordAuthenticationToken(user, null, authorities);
+
+
+            }else {
+				throw new BadCredentialsException ("Email ou Mot de passe incorrect");
+			}
+		} catch (NamingException e) {
+			throw new BadCredentialsException ("Erreur de connexion");
+		}		
+	}
+
+	@Override
+	public boolean supports(Class<?> arg0) {
+		return arg0.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
