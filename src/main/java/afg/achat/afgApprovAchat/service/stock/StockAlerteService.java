38f0295b77bab7bda320845b32b23a1277deeda3
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-12-09 15:15:02 +0300

    notif dans Etat Stock

--- src/main/java/afg/achat/afgApprovAchat/service/stock/StockAlerteService.java
+++ src/main/java/afg/achat/afgApprovAchat/service/stock/StockAlerteService.java
@@ -9,25 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class StockAlerteService {
     @Autowired
     ArticleService articleService;
+
     @Autowired
     VEtatStockService vEtatStockService;
 
+    // Méthode existante pour la page de notifications
     public StockAlerte[] getAlertes() {
-
         VEtatStock[] etats = vEtatStockService.getAllEtatStocks();
         List<StockAlerte> alertes = new ArrayList<>();
 
         for (VEtatStock etat : etats) {
-
             double stock = Double.parseDouble(etat.getStockDisponible());
             double seuil = Double.parseDouble(etat.getSeuilMin());
-
             String type = null;
 
             if (stock <= 0) {
@@ -46,7 +47,29 @@ public class StockAlerteService {
                 ));
             }
         }
-
         return alertes.toArray(new StockAlerte[0]);
+    }
+
+    // Nouvelle méthode pour obtenir les alertes par code article
+    public Map<String, StockAlerte> getAlertesMap() {
+        StockAlerte[] alertes = getAlertes();
+        Map<String, StockAlerte> alertesMap = new HashMap<>();
+
+        for (StockAlerte alerte : alertes) {
+            alertesMap.put(alerte.getCodeArticle(), alerte);
+        }
+
+        return alertesMap;
+    }
+
+    // Méthode pour obtenir le nombre total d'alertes
+    public int getAlertesCount() {
+        return getAlertes().length;
+    }
+
+    // Méthode pour vérifier si un article spécifique a une alerte
+    public StockAlerte getAlerteForArticle(String codeArticle) {
+        Map<String, StockAlerte> alertesMap = getAlertesMap();
+        return alertesMap.get(codeArticle);
     }
 }
