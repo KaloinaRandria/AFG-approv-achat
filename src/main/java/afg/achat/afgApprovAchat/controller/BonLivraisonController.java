b22148db26394f810369a3d76baef42347845cfb
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-12-22 11:11:19 +0300

    champs dynamique

--- src/main/java/afg/achat/afgApprovAchat/controller/BonLivraisonController.java
+++ src/main/java/afg/achat/afgApprovAchat/controller/BonLivraisonController.java
@@ -1,6 +1,9 @@
 package afg.achat.afgApprovAchat.controller;
 
+import afg.achat.afgApprovAchat.service.FournisseurService;
+import afg.achat.afgApprovAchat.service.util.DeviseService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/bonlivraison")
 public class BonLivraisonController {
-
+    @Autowired
+    FournisseurService fournisseurService;
+    @Autowired
+    DeviseService deviseService;
 
     @GetMapping("/list")
     public String getAllBonLivraisons(Model model) {
@@ -21,7 +27,8 @@ public class BonLivraisonController {
     public String addBonLivraisonPage(Model model, HttpServletRequest request) {
         model.addAttribute("currentUri", request.getRequestURI());
 
-
+        model.addAttribute("fournisseurs", fournisseurService.getAllFournisseurs());
+        model.addAttribute("devises", deviseService.getAllDevises());
         return "bl/bl-saisie";
     }
 }
