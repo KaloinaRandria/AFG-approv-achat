c2f67c1938a7b25e880b20b3326d0d1929555465
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-01-19 10:44:51 +0300

    Repo , Service Historique Stock

---
+++ src/main/java/afg/achat/afgApprovAchat/repository/HistoriqueMouvementStockRepo.java
@@ -0,0 +1,18 @@
+package afg.achat.afgApprovAchat.repository;
+
+import afg.achat.afgApprovAchat.model.HistoriqueMouvementStockView;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface HistoriqueMouvementStockRepo extends JpaRepository<HistoriqueMouvementStockView, String> {
+
+    // historique global
+    @Query("select h from HistoriqueMouvementStockView h order by h.dateMouvement desc")
+    List<HistoriqueMouvementStockView> findAllOrderByDateDesc();
+
+    // historique par article
+    List<HistoriqueMouvementStockView> findByIdArticleOrderByDateMouvementDesc(String idArticle);
+}
+
---
+++ src/main/java/afg/achat/afgApprovAchat/service/HistoriqueMouvementStockService.java
@@ -0,0 +1,25 @@
+package afg.achat.afgApprovAchat.service;
+
+import afg.achat.afgApprovAchat.model.HistoriqueMouvementStockView;
+import afg.achat.afgApprovAchat.repository.HistoriqueMouvementStockRepo;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class HistoriqueMouvementStockService {
+
+    private final HistoriqueMouvementStockRepo repo;
+
+    public HistoriqueMouvementStockService(HistoriqueMouvementStockRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<HistoriqueMouvementStockView> getHistoriqueGlobal() {
+        return repo.findAllOrderByDateDesc();
+    }
+
+    public List<HistoriqueMouvementStockView> getHistoriqueByArticle(String idArticle) {
+        return repo.findByIdArticleOrderByDateMouvementDesc(idArticle);
+    }
+}
+
