835db14ae6cd1238027df31df2cbd9d9c3c6837f
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-02-09 17:54:00 +0300

    Controller Bon de Sortie

--- src/main/java/afg/achat/afgApprovAchat/repository/demande/bonSortie/BonSortieMereRepo.java
+++ src/main/java/afg/achat/afgApprovAchat/repository/demande/bonSortie/BonSortieMereRepo.java
@@ -1,9 +1,13 @@
 package afg.achat.afgApprovAchat.repository.demande.bonSortie;
 
+import afg.achat.afgApprovAchat.model.demande.DemandeMere;
 import afg.achat.afgApprovAchat.model.demande.bonSortie.BonSortieMere;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BonSortieMereRepo extends JpaRepository<BonSortieMere, String> {
+    Optional<BonSortieMere> findFirstByDemandeMereOrderByIdDesc(DemandeMere demandeMere);
 }
