b79f6d5881047100e9b2a0c0340479fa796a8166
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-02-23 16:33:38 +0300

    Ajustement be

--- src/main/java/afg/achat/afgApprovAchat/configuration/MyAuthenticationProvider.java
+++ src/main/java/afg/achat/afgApprovAchat/configuration/MyAuthenticationProvider.java
@@ -48,7 +48,24 @@ public class MyAuthenticationProvider implements AuthenticationProvider{
 				return new UsernamePasswordAuthenticationToken(user, null,
 				        Collections.singletonList(new SimpleGrantedAuthority("ROLE_"+user.getRole().getLibelle())));
 			}*/
-			if(email.equals("andrianina.rakotomalala@afgbank.mg") && password.equals("mdp")) {
+            if(email.equals("famien.yao@afgbank.ci") && password.equals("mdp")) {
+                Utilisateur user = utilisateurService.getUtilisateurByMail(email);
+                Collection<GrantedAuthority> authorities = user.getRoles().stream()
+                        .map(role ->(GrantedAuthority) new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                        .toList();
+
+                return new UsernamePasswordAuthenticationToken(user, null, authorities);
+            }
+            if(email.equals("maria.razafinandraina@afgbank.mg") && password.equals("mdp")) {
+                Utilisateur user = utilisateurService.getUtilisateurByMail(email);
+                Collection<GrantedAuthority> authorities = user.getRoles().stream()
+                        .map(role ->(GrantedAuthority) new SimpleGrantedAuthority("ROLE_" + role.getRole()))
+                        .toList();
+
+                return new UsernamePasswordAuthenticationToken(user, null, authorities);
+            }
+
+            if(email.equals("andrianina.rakotomalala@afgbank.mg") && password.equals("mdp")) {
                 Utilisateur user = utilisateurService.getUtilisateurByMail(email);
                 Collection<GrantedAuthority> authorities = user.getRoles().stream()
                         .map(role ->(GrantedAuthority) new SimpleGrantedAuthority("ROLE_" + role.getRole()))
