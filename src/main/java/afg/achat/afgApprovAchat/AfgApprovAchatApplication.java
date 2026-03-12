7044d7c1cf8f262792be29da69c036774e2761b9
Yves RAKOTONDRAZAKA <yves.rakotondrazaka@afgbank.mg>
2026-03-12 11:46:07 +0300

    maj config tomcat

--- src/main/java/afg/achat/afgApprovAchat/AfgApprovAchatApplication.java
+++ src/main/java/afg/achat/afgApprovAchat/AfgApprovAchatApplication.java
@@ -2,9 +2,16 @@ package afg.achat.afgApprovAchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AfgApprovAchatApplication {
+public class AfgApprovAchatApplication extends SpringBootServletInitializer {
+
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(AfgApprovAchatApplication.class);
+	    }
 
     public static void main(String[] args) {
         SpringApplication.run(AfgApprovAchatApplication.class, args);
