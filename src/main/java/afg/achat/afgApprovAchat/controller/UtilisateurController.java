ea6287f0bab87398f2b7f7645f139edbe351b358
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-04-28 17:27:25 +0300

    Page Saisie Utilisateur

--- src/main/java/afg/achat/afgApprovAchat/controller/UtilisateurController.java
+++ src/main/java/afg/achat/afgApprovAchat/controller/UtilisateurController.java
@@ -1,10 +1,18 @@
 package afg.achat.afgApprovAchat.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAnyRole('ADMIN')")
 public class UtilisateurController {
+    @GetMapping("/add")
+    public String addUserPage(Model model) {
+        return "utilisateur/utilisateur-saisie";
+    }
 
 }
