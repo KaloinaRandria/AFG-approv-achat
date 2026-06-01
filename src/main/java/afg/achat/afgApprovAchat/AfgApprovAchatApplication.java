b87f67cb6fa4423b3e6236b2c01e5ea04d30a94b
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-06-01 11:20:33 +0300

    Ajout TimeZone

--- src/main/java/afg/achat/afgApprovAchat/AfgApprovAchatApplication.java
+++ src/main/java/afg/achat/afgApprovAchat/AfgApprovAchatApplication.java
@@ -1,9 +1,12 @@
 package afg.achat.afgApprovAchat;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class AfgApprovAchatApplication extends SpringBootServletInitializer {
@@ -12,6 +15,11 @@ public class AfgApprovAchatApplication extends SpringBootServletInitializer {
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	        return application.sources(AfgApprovAchatApplication.class);
 	    }
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Indian/Antananarivo"));
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(AfgApprovAchatApplication.class, args);
